d48ea0df4a1af583472e9ce4076849f8c7a456ec
Phongvouit <phongvoa8@gmail.com>
2025-08-25 10:26:48 +0700

    Task 24 TrungQuanDev

--- task-23.js
+++ task-23.js
@@ -2,4 +2,8 @@
  * Completed task 23
  */
 
+<<<<<<< HEAD
 // Trungquandev test conflict (from task-25)
+=======
+// Trungquandev test conflict (from task 24)
+>>>>>>> 22c2a02 (Task 24 TrungQuanDev)
